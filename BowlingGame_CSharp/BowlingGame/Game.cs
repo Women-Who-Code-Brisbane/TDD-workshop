4ef52e4647fbcc67d527f731f3d8316e291346cb
Dominique.Rolin-v <drolin@thoughtworks.com>
2016-06-30 10:21:10 +1000

    produced code at the end of the workshop

---
+++ BowlingGame_CSharp/BowlingGame/Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        private Frame [] Frames = new Frame[10];
+        private int CurrentFrame = 0;
+        private bool FirstRoll = true;
+
+        public Game()
+        {
+            for (int i = 0; i < Frames.Length; i++)
+            {
+                Frames[i] = new Frame();
+            }
+        }
+        public void Roll(int pins)
+        {
+            if (FirstRoll)
+            {
+                Frames[CurrentFrame].FirstRoll = pins;
+                FirstRoll = false;
+            }
+            else
+            {
+                Frames[CurrentFrame].SecondRoll = pins;
+                CurrentFrame++;
+                FirstRoll = true;
+            }
+
+        }
+
+        public int Score()
+        {
+            int gameScore = 0;
+            for (int frameIndex = 0; frameIndex < Frames.Length; frameIndex++)
+            {
+                int frameScore = Frames[frameIndex].FirstRoll + Frames[frameIndex].SecondRoll;
+                if (frameScore == 10)
+                {
+                    gameScore += Frames[frameIndex +1].FirstRoll;
+                }
+
+                gameScore += frameScore;
+            }
+            return gameScore;
+        }
+    }
+
+    public class Frame
+    {
+        public int FirstRoll;
+        public int SecondRoll;
+
+
+    }
+}
---
+++ BowlingGame_CSharp/BowlingGameTest1/GameTester.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameTest1
+{
+    [TestClass]
+    public class GameTester
+    {
+        private Game game;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            game = new Game();
+        }
+
+        [TestMethod]
+        public void RollAllZerosShouldGetZeroScore()
+        {
+            RollMany(20, 0);
+
+            Assert.AreEqual(0, game.Score());
+        }
+     
+        [TestMethod]
+        public void RollAllOnesShouldGetTwentyScore()
+        {
+            RollMany(20, 1);
+
+            Assert.AreEqual(20,game.Score());
+            
+        }
+
+        [TestMethod]
+        public void GameWithOneSpare()
+        {
+            game.Roll(1);
+            game.Roll(9);
+            game.Roll(5);
+            RollMany(17, 0);
+
+            Assert.AreEqual(20, game.Score());
+        }
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+    }
+}
